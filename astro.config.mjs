adb2ebdefa4d25c5d92095fae2045b10379aa991
John Shelburne <john@catfix.biz>
2024-08-12 20:13:10 +0000

    s

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://thecatfix.github.io',
+  site: 'https://thecatfix.github.io/johnshelburne/',
   base: '/johnshelburne/',
 });
