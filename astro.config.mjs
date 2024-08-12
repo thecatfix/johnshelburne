9fbc4a3dd8d7b78ac21882a4ec16e3ad3be51dbb
John Shelburne <john@catfix.biz>
2024-08-12 19:54:06 +0000

    github.dev/johnshelburne

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://johnshelburne.github.io',
+  site: 'https://github.dev/johnshelburne',
   base: '/johnshelburne',
 });
